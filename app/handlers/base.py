fc4027c40f9b6dfffc85ba7cff5c4eb83ba91470
Yuriy Chebyshev <ychebyshev@it.ru>
2020-12-17 22:51:27 +0300

    load code from old version
    (cherry picked from commit 12523cdbbffd5bc86d7edcacdb28d492e89422cb)

--- app/handlers/base.py
+++ app/handlers/base.py
@@ -43,6 +43,56 @@ async def chat_migrate(message: Message, chat: dto.Chat, dao: HolderDao):
     logger.info(f"Migrate chat from %s to %s", message.chat.id, new_id)
 
 
+
+
+
+@dp.message_handler(CommandStart(re.compile(r'^from-[\d\w_]*to-[\w_]*')))
+async def cmd_start_deep_link(message: types.Message, chat: Chat):
+    """
+        deep linking распознают строки вида:
+        http://t.me/curChangeBot?start=from-1200RUB_1500RUBto-EUR_USD
+    """
+    words = message.text.split()
+    if len(words) <= 1:
+        return await cmd_start(message)
+
+    arg = words[1]
+    if not arg.startswith('from-'):
+        return await cmd_start(message)
+
+    start_price = 5
+    end_price = arg.find('to-')
+    if not end_price:
+        return await cmd_start(message)
+    price = arg[start_price:end_price].split('_')
+    vals_to = arg[end_price + 3:].split('_')
+
+    s, _ = await Settings.get_or_create(chat=chat)
+    line = ConvertedPrices(" ".join(price), rates_source=s.get_src())
+    convert_text = line.get_only_equals_rates(vals_to)
+    if convert_text != "":
+        await message.reply(convert_text, disable_notification=True)
+    else:
+        return await cmd_start(message)
+
+
+@dp.message_handler(CommandStart())
+async def cmd_start(message: types.Message):
+    await bot.send_message(message.chat.id, config.START_MSG)
+
+
+@dp.message_handler(CommandHelp())
+async def cmd_help(message: types.Message):
+    logger.info("User {user} read help in {chat}", user=message.from_user.id, chat=message.chat.id)
+    await message.reply(config.HELP_MSG)
+
+
+@dp.message_handler(commands=["about"])
+async def cmd_about(message: types.Message):
+    await message.reply(config.ABOUT_MSG)
+
+
+
 def setup_base(dp: Dispatcher):
     dp.message.register(chat_id, commands=["idchat", "chat_id", "id"], commands_prefix="/!")
     dp.message.register(cancel_state, commands="cancel")
